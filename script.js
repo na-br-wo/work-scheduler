f4d69340c839529e5ba4c9d182412fbc5375b28f
na-br-wo <nathan.b.wood@gmail.com>
2023-04-18 01:53:07 -0600

    added inital variable and date to script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// setting up the date to display in the header
+let date = dayjs().format('dddd MMM DD YYYY')
+const currentDate = $('#currentDay')
+currentDate.text(date)
+
+// setting up save button variable
+const saveBtn = $('.saveBtn')
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
